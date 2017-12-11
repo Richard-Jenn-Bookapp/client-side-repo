b29d07a203c17cf0fc522dafb98dbc927f130cc1
Richard Livingston <livingston.richard@gmail.com>
2017-12-11 15:59:42 -0800

    we built out bookView.js, not 100% working yet. We also added a little bit to book.js, also not finished.

--- scripts/view/bookView.js
+++ scripts/view/bookView.js
@@ -4,19 +4,42 @@ var app = app || {}; // eslint-disable-line
 
 
 (function (module) {
-    let bookView = {};
+    let bookView = {}; // eslint-disable-line
 
-    bookView.initIndexPage = () => {
+    bookView.initIndexPage = (ctx) => {
         $('main section').hide();
-
-
         $('#books').empty().show();
-
         $('#books').show();
+        ctx.books.map(book => $('#books').append(book.toHtml()));
+    };
+
+    bookView.initDetailPage = (ctx) => {
+        $('main section').hide();
+        $('#books').empty().show();
+        $('#books').append(ctx.book.toHtml());
+    };
+
+    bookView.initNewPage = (ctx) => {
+        $('main section').hide();
+        $('#new-book').parent().show();
+
+        $('#new-book').one('submit', function(){
+            event.preventDefault();
+            const newBook = {
+                id: this.id.value,
+                author: this.author.value,
+                title: this.title.value,
+                isbn: this.isbn.value,
+                img_url: this.image_url.value, // re-visit?
+                description: this.description.value
+
+            };
+
+            app.Book.create(newBook);
+        });
+
 
         module.Book.all.map(book => $('#books').append(book.toHtml()));
     };
     module.bookView = bookView;
 })(app);
-
-
