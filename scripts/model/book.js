44bf0a3bb107e48aba310cf33e8ce3c86aadd1fc
Richard Livingston <livingston.richard@gmail.com>
2017-12-11 14:09:23 -0800

    added some semi colons that were required

--- scripts/model/book.js
+++ scripts/model/book.js
@@ -26,13 +26,13 @@ const API_URL = 'https://pure-cove-37929.herokuapp.com';
 
 
         $.get(`${API_URL}/api/v1/books`)
-            .done(console.log) // how is this console logging the dataObj? Cause of the chain? Weird.
+            .done(console.log); // how is this console logging the dataObj? Cause of the chain? Weird.
 
-        $.get('https://pure-cove-37929.herokuapp.com/api/v1/books')
+        $.get('https://pure-cove-37929.herokuapp.com/api/v1/books');
 
 
 
-        $.get('https://pure-cove-37929.herokuapp.com/api/v1/books')
+        $.get('https://pure-cove-37929.herokuapp.com/api/v1/books');
 
         $.get(`${API_URL}/api/v1/books`)
             .done(console.log) // how is this console logging the dataObj? Cause of the chain? Weird
