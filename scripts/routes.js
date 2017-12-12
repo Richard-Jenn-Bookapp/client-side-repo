b2722d039ae038ec6e66ca16c6779a809caef66f
Jennifer Elton <justjenny40@hotmail.com>
2017-12-12 13:21:25 -0800

    changes to routes.js file

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,12 +1,17 @@
-page('/', (ctx, next) =>{
-    app.Book.fetchAll(app.bookView.initIndexPage);
+page('/', app.Book.fetchAll, app.bookView.initIndexPage);
+
+page('/books', app.Book.fetchAll, app.bookView.initIndexPage);
+page('/books/:id', app.Book.fetchOne, app.bookView.initDetailView);
+page('/books/:id/update', app.Book.fetchOne, app.bookView.initUpdatePage);
+
+page('/new', app.bookView.initNewPage);
+
+page('/about', app.aboutView.initAboutPage);
+
+page('*',(ctx, next) => { console.log('nothing to see here');
+
 });
 
-page('/book:id', app.Book.fetchOne);
 
-page('/about', app.bookView.initIndexPage);
-
-page('*',(ctx, next) => { console.log('nothing to see here'); });
-
-
+page.base('/client-side-repo')
 page.start();
