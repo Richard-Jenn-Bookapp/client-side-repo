84ef58288d728906d159963294c9caf886751065
Jennifer Elton <justjenny40@hotmail.com>
2017-12-11 17:39:14 -0800

    added to routes-js page

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,12 +1,15 @@
-page('/', (ctx, next) =>{
-    app.Book.fetchAll(app.bookView.initIndexPage);
+page('/', app.Book.fetchAll, app.bookView.initIndexPage);
+
+page('/books', app.Book.fetchAll, app.cardView.initIndexPage);
+page('/books/:id', app.Book.fetchOne, app.bookView.initDetailPage);
+page('/books/:id/update', app.Book.fetchOne, app.bookview.initUpdatePage);
+
+page('/new', app.bookView.initNewPage);
+
+page('/about', app.bookView.initAboutPage);
+
+page('*', (ctx, next) => { console.log('nothing to see here');
+
 });
-
-page('/book:id', app.Book.fetchOne);
-
-page('/about', app.bookView.initIndexPage);
-
-page('*',(ctx, next) => { console.log('nothing to see here'); });
-
-
+page.base('/client-side-repo');
 page.start();
